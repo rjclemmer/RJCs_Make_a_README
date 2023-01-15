dbb5e1fa281eb985426f3bd24e61ec0c266a5965
Squirrely Rob <clemmer.robert.j@gmail.com>
2023-01-15 11:11:22 -0500

    started adding questions. inquirer.prompt to ask questions

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+        //validate uses ternary operator if statement to see if input is true
+        validate: (nameInput) =>
+          nameInput ? true : (console.log("Please enter a value"), false),
+      },
+      {
+        type: 'input',
+        message: 'What is the name of your Project?',
+        name: 'projectTitle',
+        validate: (nameInput) =>
+          nameInput ? true : (console.log("Please enter a value"), false),
+      },
+      {
+        type: 'input',
+        message: 'What was your motivation?',
+        name: 'description1',
+        validate: (nameInput) =>
+          nameInput ? true : (console.log("Please enter a value"), false),
+      },
+      {
+          type: 'input',
+          message: 'Why did you build this project?',
+          name: 'description2',
+          validate: (nameInput) =>
+          nameInput ? true : (console.log("Please enter a value"), false),
+      },
+      {
+          type: 'input',
+          message: 'What problem does it solve?',
+          name: 'description3',
+          validate: (nameInput) =>
+          nameInput ? true : (console.log("Please enter a value"), false),
+      },
+      {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation',
+        validate: (nameInput) =>
+        nameInput ? true : (console.log("Please enter a value"), false),
+    },
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'description3',
+        validate: (nameInput) =>
+        nameInput ? true : (console.log("Please enter a value"), false),
+    },
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'description3',
+        validate: (nameInput) =>
+        nameInput ? true : (console.log("Please enter a value"), false),
+    },
+    {
+      type: 'input',
+      message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+      name: 'installation',
+      validate: (nameInput) =>
+      nameInput ? true : (console.log("Please enter a value"), false),
+  },
+  {
+      type: 'input',
+      message: 'What problem does it solve?',
+      name: 'description3',
+      validate: (nameInput) =>
+      nameInput ? true : (console.log("Please enter a value"), false),
+  },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
